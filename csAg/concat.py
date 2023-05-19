8ed119a47cd120ab17ffb60385134d0e62491177
Philip <philipzhux@gmail.com>
2023-05-19 23:34:33 +0800

    featureFiles -> more flexiable isCodePath in extracter

--- csAg/concat.py
+++ csAg/concat.py
@@ -2,17 +2,17 @@ from .pattern import Pattern
 from .extract import Extracter
 import os
 class Concater:
-    def __init__(self, destDir, concernFiles, extracter):
+    def __init__(self, destDir, concatFiles, extracter):
         os.makedirs(destDir, exist_ok=True)
         self.destDir = destDir
-        self.concernFiles = concernFiles
+        self.concatFiles = concatFiles
         self.extracter = extracter
 
     def concat(self, output_extension):
         if not self.extracter.extracted: self.extracter.extract()
         for sid in self.extracter.getStudentList():
             with open(os.path.join(self.destDir,f"{sid}.{output_extension}"),"ab") as outFile:
-                for fn in self.concernFiles:
+                for fn in self.concatFiles:
                     # print(fn)
                     inPath = self.extracter.getCodeFilePath(sid,fn)
                     # print(inPath)
--- csAg/extract.py
+++ csAg/extract.py
@@ -1,15 +1,17 @@
 from .pattern import Pattern
 import os, zipfile, glob
 class Extracter:
-    def __init__(self, featureFiles, srcDir , destDir, pattern = Pattern(), skipExtract = False):
+    def __init__(self, isCodePath, srcDir , destDir, pattern = Pattern(), skipExtract = False, idValidator = None):
+        self.__defaultIdValidator = lambda x: str(x).isnumeric() and len(str(x))==9
         self.srcDir = srcDir
         self.destDir = destDir
         self.pattern = pattern
         self.studentList = []
         self.extracted = False
-        self.featureFiles = featureFiles
+        self.idValidator = self.__defaultIdValidator if idValidator==None else idValidator
+        self.isCodePath = isCodePath
         if skipExtract: self.skipExtract()
-    
+
     def skipExtract(self):
         for ent in glob.glob(self.destDir):
             fn = ent.split("/")[-1]
@@ -21,8 +23,8 @@ class Extracter:
     def extract(self):
         os.makedirs(self.destDir, exist_ok=True)
         for fn in os.listdir(self.srcDir):   # get the list of files
-            sid = self.pattern.parser(fn)
-            if not sid.isnumeric(): continue
+            sid = self.pattern.parse(fn)
+            if not self.idValidator(sid): continue
             self.studentList.append(sid)
             file = os.path.join(self.srcDir, fn)
             if zipfile.is_zipfile(file): # if it is a zipfile, extract it
@@ -31,23 +33,17 @@ class Extracter:
         self.extracted = True
     
     def getStudentList(self):
-        if not self.extracted: 
-            print("Please call extract() first.")
-            return None
+        assert self.extracted,"Please call extract() first."
         return self.studentList
     
     def getCodeDirPath(self,sid):
-        if not self.extracted: 
-                print("Please call extract() first.")
-                return None
+        assert self.extracted,"Please call extract() first."
         for dir,_,files in os.walk(os.path.join(self.destDir, sid)):
-            if self.featureFiles and any(fF in files for fF in self.featureFiles): return dir
+            if self.isCodePath(files): return dir
         return None
 
     def getCodeFilePath(self,sid,watchFile):
-        if not self.extracted: 
-                print("Please call extract() first.")
-                return None
+        assert self.extracted,"Please call extract() first."
         for dir,_,files in os.walk(os.path.join(self.destDir, sid)):
             for file in files:
                 if file==watchFile: return os.path.join(dir, file)
